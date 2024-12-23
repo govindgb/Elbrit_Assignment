864a93b400c75270fb683a05831bf43424042de9
govind <govind@fathershops.com>
2024-12-23 19:39:20 +0530

    project done

--- Elbrit_Assignment/src/app/page.js
+++ Elbrit_Assignment/src/app/page.js
@@ -5,7 +5,6 @@ import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Header, Footer } from '@/components'
 
-// Professional Loading Component
 const LoadingSpinner = () => (
   <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-80 z-50">
     <div className="relative">
